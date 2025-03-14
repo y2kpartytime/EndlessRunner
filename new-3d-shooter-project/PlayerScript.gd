9f0bea596a904ef015db613c9253adae1da70dd7
y2kpartytime <C24732831@myTUDublin.ie>
2025-03-14 14:33:51 +0000

    2
    2

--- new-3d-shooter-project/PlayerScript.gd
+++ new-3d-shooter-project/PlayerScript.gd
@@ -2,7 +2,11 @@ extends CharacterBody3D
 
 @export var score = 0
 @export var speed:float = 10
-@export var rot_speed =0.5
+@export var rot_speed = 1000
+@export var can_move:bool = true
+@export var gravity: float = -24.8
+@export var jump_force: float = 10.0
+
 var controlling = true
 
 var relative:Vector2 = Vector2.ZERO
@@ -18,20 +22,18 @@ func _input(event):
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		controlling = ! controlling
 
-
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass 
 
-@export var can_move:bool = true
-
-func _process(delta):
+func _physics_process(delta: float) -> void:
 	rotate(Vector3.DOWN, deg_to_rad(relative.x * deg_to_rad(rot_speed) * delta))
 	rotate(transform.basis.x,deg_to_rad(- relative.y * deg_to_rad(rot_speed) * delta))
 	relative = Vector2.ZERO
+	velocity.y += gravity * delta
+	
 	if can_move:
 		var v = Vector3.ZERO
-
 		var mult = 1
 		if Input.is_key_pressed(KEY_SHIFT):
 			mult = 3
@@ -39,7 +41,7 @@ func _process(delta):
 		var turn = Input.get_axis("Left", "Right") - v.x    
 		if abs(turn) > 0:   
 			position = position + global_transform.basis.x * speed * turn * mult * delta
-			# global_translate(global_transform.basis.x * speed * turn * mult * delta)
+			global_translate(global_transform.basis.x * speed * turn * mult * delta)
 
 		var movef = Input.get_axis("Forward", "Back")
 		if abs(movef) > 0:     
@@ -48,3 +50,10 @@ func _process(delta):
 		var upanddown = Input.get_axis("ui_up", "ui_down")
 		if abs(upanddown) > 0:     
 			global_translate(- global_transform.basis.y * speed * upanddown * mult * delta)
+
+	move_and_slide()
+	velocity.z = speed
+	
+	
+	if is_on_floor() and Input.is_action_just_pressed("ui_select"):
+		velocity.y = jump_force
